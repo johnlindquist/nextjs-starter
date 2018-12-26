197d12965422359a30f156f89920ea4486f329d6
Mohamed Meabed <mo.meabed@gmail.com>
2018-12-26 14:42:15 +0400

    WIP

--- app/mw/config.mw.js
+++ app/mw/config.mw.js
@@ -1,20 +1,21 @@
 import { IS_DEV } from "./../config";
 import { isApiUrl, isInternalUrl } from "./../helper/url";
 
-const ssDate = new Date().toJSON();
+const serverStartDate = new Date().toJSON();
 
-const getAppConfig = async (req) => {
+const getAppConfig = async () => {
 
-  return {};
+  return { serverTime: new Date().toJSON() };
 };
 
+// ssr state rendering example 
 const configMw = async (req, res, next) => {
   if (isInternalUrl(req.url) || isApiUrl(req.url)) {
     next();
     return;
   }
 
-  req.ssDate = ssDate;
+  req.ssDate = serverStartDate;
   // to implement
   req.appConfig = await getAppConfig(req);
   req.appConfig.isDev = IS_DEV;
