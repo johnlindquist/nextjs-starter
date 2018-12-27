baa1f817e4cea5e83c8680afccec0a309b3f7aa4
Mohamed Meabed <mo.meabed@gmail.com>
2018-12-27 13:40:43 +0400

    Update

--- src/Util/config.js
+++ src/Util/config.js
@@ -1,11 +1,11 @@
 //
 import { isClient } from "./cmn";
 
-export const getAppConfig = (key) => {
+export const getAppConfig = (key, defaultValue) => {
   if (!isClient) {
     return undefined;
   }
-  const appConfig = window.__NEXT_DATA__.props.appConfig || {};
+  const appConfig = window.__NEXT_DATA__.props.appConfig || defaultValue;
   return appConfig[key];
 };
 
