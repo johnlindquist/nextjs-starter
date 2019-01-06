72090bbf35f010c16ca64e505330eed5925c7320
Mohamed Meabed <mo.meabed@gmail.com>
2019-01-06 12:09:09 +0400

    Update

--- src/Util/seo.config.js
+++ src/Util/seo.config.js
@@ -1,13 +1,13 @@
 const SEO = {
-  title: "WEBSTIE_TITLE",
+  title: "WEBSITE_TITLE",
   titleTemplate: `%s | WEBSITE.COM`,
-  description: "WEBSTIE_DESCRIPTION",
+  description: "WEBSITE_DESCRIPTION",
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://website.com/",
-    title: "WEBSTIE_TITLE | WEBSITE.COM",
-    description: "WEBSTIE_DESCRIPTION",
+    title: "WEBSITE_TITLE | WEBSITE.COM",
+    description: "WEBSITE_DESCRIPTION",
     defaultImageWidth: 1200,
     defaultImageHeight: 1200,
     // Multiple Open Graph images is only available in version `7.0.0-canary.0`+ of next (see note top of README.md)
@@ -16,16 +16,16 @@ const SEO = {
         url: "https://website.com/static/img/og-image-01.jpg",
         width: 800,
         height: 600,
-        alt: "WEBSTIE_TITLE | WEBSITE.COM"
+        alt: "WEBSITE_TITLE | WEBSITE.COM"
       },
       {
         url: "https://website.com/static/img/og-image-02.jpg",
         width: 800,
         height: 600,
-        alt: "WEBSTIE_TITLE | WEBSITE.COM"
+        alt: "WEBSITE_TITLE | WEBSITE.COM"
       }
     ],
-    site_name: "WEBSTIE_TITLE | WEBSITE.COM"
+    site_name: "WEBSITE_TITLE | WEBSITE.COM"
   },
   twitter: {
     handle: "@handle",
