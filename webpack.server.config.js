debf5cbbe3cfe6bfe6ab9cb3c6714fd9e36e6bbd
Mohamed Meabed <mo.meabed@gmail.com>
2019-01-09 22:36:32 +0400

    WIP

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,4 +1,3 @@
-// const path = require('path');
 const nodeExternals = require("webpack-node-externals");
 const path = require("path");
 const webpack = require("webpack");
@@ -19,19 +18,17 @@ const commonModule = {
 
 module.exports = {
   mode: "production",
-  // externals: [/(node_modules|main\..*\.js)/],
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   entry: {
     // This is our Express server for Dynamic universal
     "next.server": path.resolve(__dirname, "app/next.server.es6.js")
   },
-  resolve: { extensions: ["html", ".js", ".ts", "jsx", "css"] },
+  resolve: { extensions: ["html", ".js", "jsx", ".ts", "jsx", "css", "sass", "scss"] },
   output: {
     // Puts the output at the root of the dist folder
     path: path.join(__dirname),
     filename: "app/[name].build.js"
   },
-
   plugins: [
     new webpack.ContextReplacementPlugin(
       // fixes WARNING Critical dependency: the request of a dependency is an expression
@@ -43,19 +40,10 @@ module.exports = {
   module: commonModule,
   devtool: false,
   target: "node",
+  // https://webpack.js.org/configuration/node/
   node: {
-    console: false,
-    global: false,
-    process: false,
-    Buffer: false,
-    fs: true,
-    __filename: false,
-    __dirname: false
+    __filename: true,
+    __dirname: true
   },
-  watch: false,
-  watchOptions: {
-    poll: 2000, // Check for changes every second
-    aggregateTimeout: 700,
-    ignored: ["files/**/*.js", "node_modules"]
-  }
+  watch: false
 };
