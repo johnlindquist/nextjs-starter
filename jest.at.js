ecffb02328069d0abaeedd633cc70eb8d23aa58b
Mohamed Meabed <mo.meabed@gmail.com>
2019-01-10 17:05:57 +0400

    Updates

--- jest.at.js
+++ jest.at.js
@@ -1,9 +1,9 @@
 // used by jest config file
 // https://www.bountysource.com/issues/41190983-syntax-error-invalid-or-unexpected-token-with-png
 const path = require("path");
-
+// jest assets transformer
 module.exports = {
-  process(src, filename, config, options) {
-    return "module.exports = " + JSON.stringify(path.basename(filename)) + ";";
+  process(src, filename) {
+    return `module.exports = ${  JSON.stringify(path.basename(filename))  };`;
   }
 };
